4227ac9313b4e6848d77f6790c9f6083a02084bd
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2022-12-06 10:44:58 +0100

    landtagswahlen aufgabe (mit allen orten)

--- CSV-Dateien/UE3/landtagswahlen_advanced.py
+++ CSV-Dateien/UE3/landtagswahlen_advanced.py
@@ -1,19 +1,26 @@
 '''
-Datenanalyse der Landtagswahlen in Vomp Tirol
+Datenanalyse der Landtagswahlen (mit UI)
 Autor: Lucas Grundl
 Datum: 24.10.2021
 '''
 
 import matplotlib.pyplot as plt
+import csv
+
+gemeinden = []
+wahlen_2018 = open("2018.csv", "r") # Datei öffnen
+
+
+
 
 # Liest die alle daten des ortes Vomp aus
-def get_vomp(file):
+def get_ort(file,ort):
     file = open(file, "r") # Datei öffnen
     data = [] # Liste erstellen
 
     # Daten in einzelne Liste speichern (splitten)
     for i in file:
-        if "Vomp" in i:
+        if ort in i:
             data = i.split(";")
     file.close()
 
@@ -31,14 +38,25 @@ def get_wahlbeteiligung(data):
 jahre = [1989, 1994, 1999, 2003, 2008, 2013, 2018] # Jahre in denen gewählt wurde
 wahlbeteiligung = [] # Liste für die Wahlbeteiligung
 
+# Lade alle gemeiden in eine liste
+wh = csv.reader(wahlen_2018, delimiter=";")
+i = 0
+for line in wh:
+    if i > 1:
+        gemeinden.append(line[3])
+    i += 1
+wahlen_2018.close()
+
+uiOrt = input("Ort: ")
+
 for i in jahre:
     # fügt die wahlbeteiligung des jahres in die liste ein
-    wahlbeteiligung.append(get_wahlbeteiligung(get_vomp("{}.csv".format(i))))
+    wahlbeteiligung.append(get_wahlbeteiligung(get_ort("{}.csv".format(i), uiOrt)))
 
 
 
 # Plot
-plt.title("Wahlbeteiligung in Vomp Tirol", fontsize=20) # Titel
+plt.title("Wahlbeteiligung in {} Tirol".format(uiOrt), fontsize=20) # Titel
 plt.ylim(0, 100) # Y-Achse von 0 bis 100
 plt.plot(jahre, wahlbeteiligung, label="Wahlbeteiligung") # Daten
 plt.show() # Diagramm anzeigen
