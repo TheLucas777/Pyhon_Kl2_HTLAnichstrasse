c7b60ebfbd721ace1b3358a60c371cdf72ef0bc8
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2023-01-12 15:45:48 +0100

    UE4 fertig und kommentiert

--- CSV-Dateien/UE4/csv_table_format.py
+++ CSV-Dateien/UE4/csv_table_format.py
@@ -5,22 +5,34 @@ Datum: 10.01.23
 '''
 
 # Importe
-from tkinter import filedialog as fd    # fd.askopenfile() lässt den user eine datei auswählen
+from tkinter import filedialog as fd  # fd.askopenfile() lässt den user eine datei auswählen
+import csv  # csv.reader() liest die datei aus (besser als normales open(), da automatisch
+            # getrennt wird)
 
-
+# with open() schießt datei automatisch
 with open(fd.askopenfilename()) as file:
-    splitchar = input("Bitte gib an wie die CSV Datei getrennt ist: ")  # Trennzeichen
-    #data = file.readlines() # Liest alle Zeilen der Datei
-    for line in file:
-        print(len(line)*"-")
-        print(line.strip("\n").split(splitchar))
+    csv_f = csv.reader(file)
 
+    # Zeilen auslesen
+    for row in csv_f:
 
-#daten verarbeiten und formatieren
+        # anhand der einträge ----- strich erstellen
+        for i in row:
+            print(16 * "-", end="")
+        print("")  # nächste Zeile
 
-#for i in range(len(data)):
-#    print(len(data) * "-")
-#    spalte = data[i].split(splitchar) # Splittet die Zeile an den Semikolons
-#    for j in range(len(spalte)):
-#        print(f"|{spalte[j]}", end="")
-#    print("|",end="")
+        # Einträge formatieren Zahlen rechtsbündig und Text linksbündig
+        for i in row:
+            i = i.strip(" ")
+
+            try:
+                i = int(i)  # versucht i in int umzuwandeln
+                print(f"{i:>15.15}", end="|")  # wenn dies möglich dann rechtsbündig anzeigen
+            except ValueError:  # wenn nicht
+                try:
+                    i = float(i)  # versucht i in float umzuwandeln
+                    print(f"{i:>15.15}", end="|")  # wenn dies möglich dann rechtsbündig anzeigen
+
+                except ValueError:  # wenn nicht
+                    print(f"{i:<15.15}", end="|")  # dann linksbündig anzeigen
+        print("") # nächste Zeile
